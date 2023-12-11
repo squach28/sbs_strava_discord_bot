83688fee3769c5f88ad187f74fa2eb0232287bf6
squach28 <squach3443@gmail.com>
2023-12-11 15:46:35 -0800

    stats command completed for server + bot dm

--- client/commands/stats/stats.js
+++ client/commands/stats/stats.js
@@ -1,5 +1,22 @@
 const { SlashCommandBuilder } = require('discord.js')
 const { getStatsByDiscordId } = require('../../../handlers/statsHandler')
+const MAX_CHARACTER_LENGTH = 42
+
+const createStatsTable = (name, stats) => {
+    const table = []
+    table.push(`Stats for ${name} \n`)
+    const divider = '-'.repeat(MAX_CHARACTER_LENGTH) + "\n"
+    table.push(divider)
+    for(let stat of stats) {
+        table.push(`${stat.category} - ${stat.numOfActivities} activities, ${stat.distance} mi\n`)
+        table.push(divider)
+    }
+    const totalDistance = stats.reduce((accum, item) => accum += item.distance, 0)
+    const totalActivities = stats.reduce((accum, item) => accum += item.numOfActivities, 0)
+    const summary = `Total Activities: **${totalActivities}**, Total Distance: **${totalDistance} mi**`
+    table.push(summary)
+    return table
+}
 
 module.exports = {
    data: new SlashCommandBuilder()
@@ -10,7 +27,8 @@ module.exports = {
         const discordId = user.id 
         try {
             const stats = await getStatsByDiscordId(discordId)
-            await interaction.reply(stats.recent_run_totals.distance.toString())
+            const statsTable = createStatsTable(user.username, stats)
+            await interaction.reply(statsTable.join(''))
         } catch(e) {
             console.log(e);
             await interaction.reply('Something went wrong, please try again later.')
@@ -21,7 +39,8 @@ module.exports = {
         const discordId = user.id 
         try {
             const stats = await getStatsByDiscordId(discordId)
-            await user.send(stats.recent_run_totals.distance.toString())
+            const statsTable = createStatsTable(user.username, stats)
+            await user.send(statsTable.join(''))
         } catch(e) {
             await user.send('Something went wrong, please try again later.')
         }
