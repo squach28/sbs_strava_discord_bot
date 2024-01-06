1a64017f38b3d3fbdbb86eaa7cdf003d93fce9b9
squach28 <squach3443@gmail.com>
2024-01-05 20:37:33 -0800

    added / endpoint for job to call so web server doesn't go inactive

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+    res.status(200).json({ message: 'success' })
+})
+
 app.listen(PORT, () => {
     discordClient.login(process.env.DISCORD_TOKEN)
     .then(() => {
